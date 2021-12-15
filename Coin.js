3e639effc0296289c7f7106b6652729e126072c3
Md Ishaque <ishaqrabbu97@gmail.com>
2021-12-15 23:20:22 +0600

    first commit

---
+++ Coin.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function Coin({
+  id,
+  image,
+  cryptoName,
+  cryptoSymbol,
+  price,
+  volume,
+  priceChange,
+  marketCap,
+}) {
+  return (
+    <>
+      <div className="coin-container">
+        <div className="coin-column">
+          <div className="coin-row">
+            <div className="coin">
+              <p> {id} </p>{' '}
+              <img src={image} alt="Crypto" className="coin img" />
+              <h1 className="coin text"> {cryptoName} </h1>{' '}
+              <p className="coin symbol"> {cryptoSymbol} </p>{' '}
+            </div>{' '}
+            <div className="coinData">
+              <p className="coinData price"> $ {price.toLocaleString()} </p>{' '}
+              <p className="coinData volume"> $ {volume.toLocaleString()} </p>{' '}
+              {priceChange < 0 ? (
+                <p className="coinData percent red">
+                  {' '}
+                  {priceChange.toFixed(2)} %
+                </p>
+              ) : (
+                <p className="coinData percent green">
+                  {' '}
+                  {priceChange.toFixed(2)} %
+                </p>
+              )}
+              <p className="coinData marketCap">
+                $ {marketCap.toLocaleString()}{' '}
+              </p>{' '}
+            </div>{' '}
+          </div>{' '}
+        </div>{' '}
+      </div>{' '}
+    </>
+  )
+}
---
+++ CryptoTracker.js
@@ -0,0 +1,80 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Coin from './Coin'
+
+export default function CryptoTracker() {
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    const makeRequest = async (config) => {
+      return await axios(config)
+    }
+    const getData = () => {
+      makeRequest({
+        url:
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false',
+        method: 'GET',
+      })
+        .then((res) => setCoins(res.data))
+        .catch((err) => console.log(err))
+    }
+    getData()
+  }, [])
+
+  const changeHandler = (e) => {
+    setSearch(e.target.value)
+  }
+  const filterCoin = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase()),
+  )
+  return (
+    <>
+      <div className="coinApp">
+        <div className="coinApp search">
+          <h1 className="coinApp text">Search a currency</h1>
+          <form action="">
+            <input
+              type="text"
+              onChange={changeHandler}
+              placeholder="Search"
+              className="coinApp input"
+            />
+          </form>
+        </div>
+        <div className="coin-container titles">
+          <div className="coin-column">
+            <div className="coin-row titles">
+              <div className="coin ">
+                <p>#</p>
+                <h1 className="coin text titles">Coin</h1>
+              </div>
+              <div className="coinData titles">
+                <p className="coinData price">Price</p>
+                <p className="coinData volume">24h Volume</p>
+                <p className="coinData percent">24h</p>
+                <p className="coinData marketCap">Mkt Cap</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {filterCoin.map((coin, index) => {
+          return (
+            <Coin
+              key={coin.id}
+              id={index + 1}
+              image={coin.image}
+              cryptoName={coin.name}
+              cryptoSymbol={coin.symbol}
+              price={coin.current_price}
+              marketCap={coin.market_cap}
+              priceChange={coin.price_change_percentage_24h}
+              volume={coin.total_volume}
+            />
+          )
+        })}
+      </div>
+    </>
+  )
+}
